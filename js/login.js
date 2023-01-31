445388e260137781f7dd5dbd5d372ad9655624be
nuhash123 <mdnuhashimroz@gmail.com>
2023-01-31 16:07:25 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,9 @@
+document.getElementById('button-submit').addEventListener('click', function () {
+    const getEmail = document.getElementById('get-email');
+    const email = getEmail.value;
+    console.log(email);
+    const getPassword = document.getElementById('get-password');
+    const password = getPassword.value;
+    console.log(password);
+    document.getElementById('button-submit').addEventListener("click", function ()
+});
